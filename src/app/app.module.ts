3128a041fad45316dfc79afe31214f8846237f9b
Aidan Starr <aidan.starr19@gmail.com>
2020-07-03 01:04:19 -0400

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToDoComponent } from './to-do/to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ToDoComponent]
 })
 export class AppModule { }
---
+++ src/app/to-do/to-do.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+interface Todo {
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css']
+})
+export class ToDoComponent{
+
+  tasks: Todo[]= [
+    {task: "Eat food", completed: true},
+    {task: "Pet dogs", completed: true},
+    {task: "sleep", completed: false},
+    {task: "Play soccer", completed: false}
+  ];
+
+  completeCheck = (el: Todo): boolean => el.completed === false;
+  emptyList = (): boolean => this.tasks.length === 0;
+
+  stylesList: object = {
+    "text-decoration":"line-through"
+  }
+
+}
